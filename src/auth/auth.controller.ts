66242fbf682fbb62260f5bbaaeb3064e024508f8
carloncho <qarloztapia@gmail.com>
2024-08-28 21:31:31 -0400

    mjikol

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards, Request, UsePipes, ValidationPipe, Res } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards, Request, UsePipes, ValidationPipe, Res, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -19,20 +19,21 @@ export class AuthController {
       
       res.cookie('access_token', result.access_token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development', // true en producción
-        sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
-        domain: process.env.NODE_ENV === 'development' ? 'localhost' : '.adaptable.app',
+        secure: true,
+        sameSite: 'none',
+        domain: process.env.NODE_ENV === 'production' ? '.adaptable.app' : undefined,
         path: '/',
         maxAge: 24 * 60 * 60 * 1000, // 1 día
       });
     
       return { success: true };
     }
-
     @Get('check')
     @UseGuards(AuthGuard('jwt'))
-    checkAuth() {
-      return { isAuthenticated: true };
+    checkAuth(@Req() req) {
+      console.log('Cookie recibida:', req.headers.cookie);
+      console.log('Usuario autenticado:', req.user);
+      return { authenticated: true, user: req.user };
     }
 
     @UseGuards(AuthGuard('jwt'))
